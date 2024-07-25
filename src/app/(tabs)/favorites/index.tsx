ddbcd9a5eae41f6a8f7350204eeb5abc7e5b6f9b
claudiaaziz <claudiahannahh@gmail.com>
2024-07-25 15:34:11 -0400

    Begin FavoritesScreen

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,11 +1,29 @@
+import library from '@/assets/data/library.json'
+import TrackList from '@/components/TrackList'
+import { screenPadding } from '@/constants/tokens'
+import useNavigationSearch from '@/hooks/useNavigationSearch'
 import { defaultStyles } from '@/styles'
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useMemo } from 'react'
+import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function FavoritesScreen() {
+	const search = useNavigationSearch({
+		searchBarOptions: { placeholder: 'Find in songs' },
+	})
+    
+	const favoriteTracks = useMemo(() => {
+		return library.filter((track) => track.rating)
+	}, [])
+
 	return (
 		<View style={defaultStyles.container}>
-			<Text style={defaultStyles.text}>Favorites screen</Text>
+			<ScrollView
+				style={{ paddingHorizontal: screenPadding.horizontal }}
+				contentInsetAdjustmentBehavior="automatic"
+			>
+				<TrackList scrollEnabled={false} tracks={favoriteTracks} />
+			</ScrollView>
 		</View>
 	)
 }
