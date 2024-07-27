1a6f5d871e137da8b13a4cd1861caeec96a8a4e4
claudiaaziz <claudiahannahh@gmail.com>
2024-07-26 23:48:35 -0400

    Add artist details screen

--- src/app/(tabs)/artists/_layout.tsx
+++ src/app/(tabs)/artists/_layout.tsx
@@ -1,4 +1,5 @@
 import { StackScreenWithSearchBar } from '@/constants/layout'
+import { colors } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
@@ -7,7 +8,19 @@ export default function ArtistsScreenLayout() {
 	return (
 		<View style={defaultStyles.container}>
 			<Stack>
-				<Stack.Screen name="index" options={{ ...StackScreenWithSearchBar, headerTitle: 'Artists' }} />
+				<Stack.Screen
+					name="index"
+					options={{ ...StackScreenWithSearchBar, headerTitle: 'Artists' }}
+				/>
+				<Stack.Screen
+					name="[name]"
+					options={{
+						headerTitle: '',
+						headerBackVisible: true,
+						headerStyle: { backgroundColor: colors.background },
+						headerTintColor: colors.primary,
+					}}
+				/>
 			</Stack>
 		</View>
 	)
